b2b36fa5ab2b930279004ddf2e6e883b87d63f60
LRVT <21357789+l4rm4nd@users.noreply.github.com>
2022-04-05 23:26:30 +0200

    Update entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "[~] Makemigrations and migrate"
-python manage.py makemigrations
+python manage.py makemigrations polls
 python manage.py migrate
 echo "[~] Change ownership of db.sqlite3 to www-data"
 chown www-data: /opt/mysite/database/db.sqlite3
